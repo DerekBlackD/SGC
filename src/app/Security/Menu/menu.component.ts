66ca8a182c165435afaf485e6d99411749497f37
DerekBlackD <knljnr@gmail.com>
2017-05-17 01:59:31 -0500

    Animación de Menu

--- src/app/Security/Menu/menu.component.ts
+++ src/app/Security/Menu/menu.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
     selector: 'menu-component',
     templateUrl: 'menu.component.html',
-    styleUrls: ['menu.component.css']
+    styleUrls: ['menu.component.css'],
+    animations: [trigger(
+      'openClose',
+      [
+        state('collapsed, void', style({ left: -282})),
+        state('expanded', style({left: 0})),
+        transition(
+            'collapsed <=> expanded', [animate(0, style({})), animate(500)])
+      ])],
 })
-export class MenuComponent{ }
+export class MenuComponent{
+    stateExpression: string;
+    flag: boolean;
+    constructor() { 
+        this.collapse();
+        this.flag = true;
+    }
+    expand() { 
+        this.stateExpression = 'expanded'; 
+    }
+    collapse() { 
+        this.stateExpression = 'collapsed'; 
+    }
+    ShowHideMenu():void{
+        if(this.flag){
+            this.expand();
+            this.flag = !this.flag;
+        }else{
+            this.collapse();
+            this.flag = !this.flag;
+        }
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './Security/Banner/banner.component';
@@ -16,7 +17,8 @@ import { MenuComponent } from './Security/Menu/menu.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
