65176c2d6fd413f9dc6812e5c16cd83e1946966c
DerekBlackD <knljnr@gmail.com>
2017-08-21 04:59:15 -0500

    Correccion

--- src/app/Collection/Process/GeneralManagement/GenManagement/genmanagement.component.ts
+++ src/app/Collection/Process/GeneralManagement/GenManagement/genmanagement.component.ts
@@ -33,18 +33,22 @@ export class GenManagement{
 
      constructor(private _collectionService: CollectionService,
                 private _util: UtilitesService) {
-         this.oManagement.ResultID = '';
-         this.oManagement.LocationContact = '';
-         this.oManagement.ContactID = '';
-         this.oManagement.TypeComp = '';
-         this.oManagement.DateCompString = _util.getDate();
-         this.oManagement.Currency = '';
-         this.oManagement.Amount = '0.00';
-         this.oManagement.MngtType = 1;
-         this.startDate = _util.getDateTime();
-         this.todayDate = _util.getDate();
+         this.resetVariables();
          this.loadResult();
          this.loadData();
+     }
+
+     resetVariables(): void {
+        this.oManagement.ResultID = '';
+        this.oManagement.LocationContact = '';
+        this.oManagement.ContactID = '';
+        this.oManagement.TypeComp = '';
+        this.oManagement.DateCompString = this._util.getDate();
+        this.oManagement.Currency = '';
+        this.oManagement.Amount = '0.00';
+        this.oManagement.MngtType = 1;
+        this.startDate = this._util.getDateTime();
+        this.todayDate = this._util.getDate();
      }
 
      loadResult(): void {
@@ -103,11 +107,10 @@ export class GenManagement{
 
         this.oManagement.CustomerBagID = this.customerData.CustomerBagID;
         this.oManagement.CustomerID = this.customerData.CustomerID;
-        this.oManagement.AgentID = 0;
+        this.oManagement.AgentID = 1;
         this.oManagement.BagID = this.customerData.BagID;
         this.oManagement.MngtDateString = this._util.getDateTime();
         this.oManagement.MngtClass = '1';
-        this.oManagement.MngtType = '';
         this.oManagement.AgentTypist = 1;
         this.oManagement.AddressID = 0;
         this.oManagement.PhoneID = this.selectPhone.phoneID;
@@ -122,6 +125,7 @@ export class GenManagement{
         this._collectionService.getAllDataByID('api/customerbag/postcustbagmanagement', this.oManagement)
             .subscribe(result => {
                 this.loadManagements();
+                this.resetVariables();
                 this.todayDate = this._util.getDate();
                 this.blockUI.stop();
             })
